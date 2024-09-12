fe55e2622afcdfa652440a2bd5a7da9487970010
GidyMane <muragegideon2000@gmail.com>
2024-09-12 18:10:20 +0300

    update

---
+++ src/app/callpro/jobs/page.tsx
@@ -0,0 +1,12 @@
+import { Jobview } from '@/components/Jobs/Jobview'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>      
+      <Jobview/>
+    </div>
+  )
+}
+
+export default page
