94ce331276868ce8e41ba1a790f0c1159060eeb5
GidyMane <muragegideon2000@gmail.com>
2024-10-26 16:21:05 +0300

    constants update

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,3 +1,3 @@
-export const baseUrl= "http://localhost:8000/api/v1/"
-// export const baseUrl= "https://field-service-management.vercel.app/api/v1/"
+// export const baseUrl= "http://localhost:8000/api/v1/"
+export const baseUrl= "https://field-service-management.vercel.app/api/v1/"
  
